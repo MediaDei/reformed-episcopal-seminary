2ab96af087691341109378f2c3a3202ec9127abe
Phasstw <phasstw@gmail.com>
2017-09-01 17:06:45 -0400

    updates to Heritage, Header, jQuery Counter, started global banners

--- footer.php
+++ footer.php
@@ -2,10 +2,30 @@
 		<div class="mediadei">
 			&copy; <?php echo date("Y"); ?>, ORGANIZATION | <a href="http://www.mediadei.org">Designed and built by Media Dei</a>
 		</div>
-
+    <script type="text/javascript">
+      //jQuery Count-Up Script
+      jQuery(function ($) {  
+        $('.counter').each(function() {
+          var $this = $(this),
+              countTo = $this.attr('data-count');
+          $({ countNum: $this.text()}).animate({
+            countNum: countTo
+          },
+          {
+            duration: 4000,
+            easing:'linear',
+            step: function() {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function() {
+              $this.text(this.countNum);
+              //alert('finished');
+            }
+          });  
+        });
+      });
+    </script>
 	</footer>
-
-
 	<?php wp_footer(); ?>
 </body>
 </html>
--- page-heritage.php
+++ page-heritage.php
@@ -1,24 +1,50 @@
 <?php get_header(); ?>
 <section class="heritage">
-  <h2>Founded on Scripture</h2>
+  <h2 class="p-heading">Founded on Scripture</h2>
   <p class="drop-sc">
     Since 1887, the seminary has taught and expounded on the docrines of Scripture. Quisque volutpat augue enim, pulvinar lobortis nibh lacinia at. Vestibulum nec erat ut mi sollicitudin porttitor id sit amet risus. Nam tempus vel odio vitae aliquam. In imperdiet eros id lacus vestibulum vestibulum. Suspendisse fermentum sem sagittis ante venenatis egestas quis vel justo. Maecenas semper suscipit nunc, sed aliquam sapien convallis eu. Nulla ut turpis in diam dapibus consequat.
   </p>
   <div class="photo"></div>
 
-  <h2>Ensconced in Scholarship</h2>
+  <h2 class="p-heading">Ensconced in Scholarship</h2>
   <p class="drop-sc">
     Nam porttitor blandit accumsan. Ut vel dictum sem, a pretium dui. In 
     malesuada enim in dolor euismod, id commodo mi consectetur. Curabitur at vestibulum nisi. Nullam vehicula nisi velit. Mauris turpis nisl, molestie ut ipsum et, suscipit vehicula odio. Vestibulum interdum vestibulum felis ac molestie. Praesent aliquet quam et libero dictum, vitae dignissim leo eleifend. In in turpis turpis. Quisque justo turpis, vestibulum non enim nec, tempor mollis mi.
   </p>
   <div class="photo"></div>
 
-  <h2>Faithful In Adversity</h2>
+  <h2 class="p-heading">Faithful In Adversity</h2>
   <p class="drop-sc">
     Ut vel dictum sen, a pretium dui. In malesuada enim in dolor euismod, 
     id commodo mi consectetur. Curabitur at vestibulum nisi. Nullam vehicula nisi velit. Mauris turpis nisl, molestie ut ipsum et, suscipit vehicula odio. Vestibulum interdum vestibulum felis ac molestie. Praesent aliquet quam et libero dictum, vitae dignissim leo eleifend. In in turpis turpis. Quisque justo turpis, vestibulum non enim nec, tempor mollis mi. Sed vel tristique.
   </p>
   <div class="photo"></div>
+  <div class="statistics grid">
+    <h2>A History of Impact</h2>
+    <div class="grid-1-3">
+      <div class="counter" data-count="4000">0</div>
+      <p>Alumni<br>Graduated</p>
+    </div>
+    <div class="grid-1-3">
+      <div class="counter" data-count="300">0</div>
+      <p>Clergy<br>Placed</p>
+    </div>
+    <div class="grid-1-3">
+      <div class="counter" data-count="60">0</div>
+      <p>Churches<br>Served</p>
+    </div>
+  </div>
+  <div class="founded-banner grid">
+    <img src="/wp-content/themes/res-theme/images/logo-white-x2.png" alt="logo">
+    <div class="grid-1-2">
+      <p class="top">seminary founded</p>
+      <p class="bottom">In the year of our lord</p>
+    </div>
+    <div class="grid-1-2">
+      <p class="date">1887<time datetime="1887"></p>
+    </div>
+  </div>
   <div class="bottom-photo"></div>
 </section>
+
 <?php get_footer(); ?>
