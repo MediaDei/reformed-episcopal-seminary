9e09d3ae2d965c9596fb36002f3264e729f4762e
Phasstw <phasstw@gmail.com>
2017-09-14 01:51:52 -0400

    started and finished programs skeleton

---
+++ page-programs.php
@@ -0,0 +1,148 @@
+<?php get_header(); ?>
+<section class="programs">
+  <div class="program">
+    <h2 class="program-title">Master of Divinity</h2>
+    <p class="short-desc">
+      Cras quis nulla commodo, aliquam lectus sed, blandit augue. Cras ullamcorper bibendum bibendum. Duis tin.
+    </p>
+    <img class="program-img" src="/wp-content/themes/res-theme/images/program-1.jpg" alt="students">
+    <div class="statistics grid">
+      <div class="grid-1-3">
+        <p class="number">3</p>
+        <p>Years of<br>Study</p>
+      </div>
+      <div class="grid-1-3">
+        <p class="number">9</p>
+        <p>Foundational<br>Courses</p>
+      </div>
+      <div class="grid-1-3">
+        <p class="number">27</p>
+        <p>Credit hours of<br>course work</p>
+      </div>
+    </div>
+    <p class="long-desc drop-sc">
+      Ut vel dictim sem, a pretium dui. In malesuada enim in dolor euismod, 
+      id commodo mi consectetur. Curabitur at vestibulum nisi. Nullam vehicula nisi velit. Mauris turpis nisl, molestie ut ipsum et, suscipit vehicula odio. Vestibulum interdum vestibulum felis ac molestie. Praesent aliquet quam et libero dictum, vitae dignissim leo eleifend. In in turpis turpis. Quisque justo turpis, vestibulum non enim nec, tempor mollis mi. Sed vel tristique.
+    </p>
+    <div class="connect-banner grid">
+      <img src="/wp-content/themes/res-theme/images/logo-white-x2.png" alt="logo">
+      <div class="grid-2-3">
+        <p class="top">Connect with us</p>
+        <p class="bottom">Explore finaincial aid, and start the conversation</p>
+      </div>
+      <div class="grid-1-3">
+        <a class="button" href="/contact">Contact</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="program">
+    <h2 class="program-title">Certificate in Bible and Theology</h2>
+    <p class="short-desc">
+      Cras quis nulla commodo, aliquam lectus sed, blandit augue. Cras ullamcorper bibendum bibendum. Duis tin.
+    </p>
+    <img class="program-img" src="/wp-content/themes/res-theme/images/program-2.jpg" alt="students">
+    <div class="statistics grid">
+      <div class="grid-1-3">
+        <p class="number">3</p>
+        <p>Years of<br>Study</p>
+      </div>
+      <div class="grid-1-3">
+        <p class="number">9</p>
+        <p>Foundational<br>Courses</p>
+      </div>
+      <div class="grid-1-3">
+        <p class="number">27</p>
+        <p>Credit hours of<br>course work</p>
+      </div>
+    </div>
+    <p class="long-desc drop-sc">
+      Ut vel dictim sem, a pretium dui. In malesuada enim in dolor euismod, 
+      id commodo mi consectetur. Curabitur at vestibulum nisi. Nullam vehicula nisi velit. Mauris turpis nisl, molestie ut ipsum et, suscipit vehicula odio. Vestibulum interdum vestibulum felis ac molestie. Praesent aliquet quam et libero dictum, vitae dignissim leo eleifend. In in turpis turpis. Quisque justo turpis, vestibulum non enim nec, tempor mollis mi. Sed vel tristique.
+    </p>
+    <div class="connect-banner grid">
+      <img src="/wp-content/themes/res-theme/images/logo-white-x2.png" alt="logo">
+      <div class="grid-2-3">
+        <p class="top">Connect with us</p>
+        <p class="bottom">Explore finaincial aid, and start the conversation</p>
+      </div>
+      <div class="grid-1-3">
+        <a class="button" href="/contact">Contact</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="program">
+    <h2 class="program-title">Continuing Education for the Church</h2>
+    <p class="short-desc">
+      Cras quis nulla commodo, aliquam lectus sed, blandit augue. Cras ullamcorper bibendum bibendum. Duis tin.
+    </p>
+    <img class="program-img" src="/wp-content/themes/res-theme/images/program-3.jpg" alt="students">
+    <div class="statistics grid">
+      <div class="grid-1-3">
+        <p class="number">3</p>
+        <p>Years of<br>Study</p>
+      </div>
+      <div class="grid-1-3">
+        <p class="number">9</p>
+        <p>Foundational<br>Courses</p>
+      </div>
+      <div class="grid-1-3">
+        <p class="number">27</p>
+        <p>Credit hours of<br>course work</p>
+      </div>
+    </div>
+    <p class="long-desc drop-sc">
+      Ut vel dictim sem, a pretium dui. In malesuada enim in dolor euismod, 
+      id commodo mi consectetur. Curabitur at vestibulum nisi. Nullam vehicula nisi velit. Mauris turpis nisl, molestie ut ipsum et, suscipit vehicula odio. Vestibulum interdum vestibulum felis ac molestie. Praesent aliquet quam et libero dictum, vitae dignissim leo eleifend. In in turpis turpis. Quisque justo turpis, vestibulum non enim nec, tempor mollis mi. Sed vel tristique.
+    </p>
+    <div class="connect-banner grid">
+      <img src="/wp-content/themes/res-theme/images/logo-white-x2.png" alt="logo">
+      <div class="grid-2-3">
+        <p class="top">Connect with us</p>
+        <p class="bottom">Explore finaincial aid, and start the conversation</p>
+      </div>
+      <div class="grid-1-3">
+        <a class="button" href="/contact">Contact</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="program">
+    <h2 class="program-title">Licentiate for Diaconal Minister</h2>
+    <p class="short-desc">
+      Cras quis nulla commodo, aliquam lectus sed, blandit augue. Cras ullamcorper bibendum bibendum. Duis tin.
+    </p>
+    <img class="program-img" src="/wp-content/themes/res-theme/images/program-4.jpg" alt="students">
+    <div class="statistics grid">
+      <div class="grid-1-3">
+        <p class="number">3</p>
+        <p>Years of<br>Study</p>
+      </div>
+      <div class="grid-1-3">
+        <p class="number">9</p>
+        <p>Foundational<br>Courses</p>
+      </div>
+      <div class="grid-1-3">
+        <p class="number">27</p>
+        <p>Credit hours of<br>course work</p>
+      </div>
+    </div>
+    <p class="long-desc drop-sc">
+      Ut vel dictim sem, a pretium dui. In malesuada enim in dolor euismod, 
+      id commodo mi consectetur. Curabitur at vestibulum nisi. Nullam vehicula nisi velit. Mauris turpis nisl, molestie ut ipsum et, suscipit vehicula odio. Vestibulum interdum vestibulum felis ac molestie. Praesent aliquet quam et libero dictum, vitae dignissim leo eleifend. In in turpis turpis. Quisque justo turpis, vestibulum non enim nec, tempor mollis mi. Sed vel tristique.
+    </p>
+    <div class="connect-banner grid">
+      <img src="/wp-content/themes/res-theme/images/logo-white-x2.png" alt="logo">
+      <div class="grid-2-3">
+        <p class="top">Connect with us</p>
+        <p class="bottom">Explore finaincial aid, and start the conversation</p>
+      </div>
+      <div class="grid-1-3">
+        <a class="button" href="/contact">Contact</a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
