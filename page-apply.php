a216de31f30a332bdb0fe2cd2086bc9600e7174d
Phasstw <phasstw@gmail.com>
2017-10-12 17:40:21 -0400

    started apply.php skeleton

---
+++ page-apply.php
@@ -0,0 +1,48 @@
+<?php get_header(); ?>
+<section class="apply">
+  <div class="application-step">
+    <div class="number offset">1</div>
+    <div class="info">
+      <h2>Step One</h2>
+      <p>
+        <span class="italic">The application is where you begin.</span> Download the application below, and fill out all of the necessary fields. Make sure to enter your personal details, and where necessary do lorem ipsum. Vestibulum rutrum quam vitae fringilla tincidunt. Ut laoreet sodales nisi, quis iaculis nulla iaculis vitae. 
+      </p>
+      <a class="application-dl">Download Application</a>
+    </div>
+  </div>
+
+  <div class="application-step offset">
+    <div class="number">2</div>
+    <div class="info">
+      <h2>Step Two</h2>
+      <p>
+        <span class="italic">Next you need two references.</span> Download both reference forms below. One will be your personal reference letter, and the other a reference letter from your pastor. Lorem ipsum dolor sit amet. Arma virumque cano. Duis tincidunt urna non pretium porta. Nam condimentum vitae ligula vel ornare. 
+      </p>
+      <a class="application-dl">Personal Reference Letter</a>
+      <a class="application-dl">Personal Reference Letter</a>
+    </div>
+  </div>
+
+  <div class="application-step">
+    <div class="number offset">3</div>
+    <div class="info">
+      <h2>Step Three</h2>
+      <p>
+        <span class="italic">Then write your essays.</span> Here is where you get a chance to express what motivates you to apply, your values and life goals, as well as the academic interests you would like to pursue. Vestibulum rutrum quam vitae fringilla tincidunt. Ut laoreet sodales nisi, quis iaculis nulla iaculis vitae.  
+      </p>
+      <a class="application-dl">Essay Requirements</a>
+    </div>
+  </div>
+
+  <div class="application-step offset">
+    <div class="number">4</div>
+    <div class="info">
+      <h2>Step Four</h2>
+      <p>
+        <span class="italic">Finally send us your transcripts.</span> Use a form such as the one below to request transcripts from the academic institutions you’ve previously studied at. Lorem ipsum dolor sit amet. Arma virumque cano. Duis tincidunt urna non pretium porta. Nam condimentum vitae ligula vel ornare.
+      </p>
+      <a class="application-dl">Transcript Request Form</a>
+    </div>
+  </div>
+</section>
+<?php get_footer(); ?>
