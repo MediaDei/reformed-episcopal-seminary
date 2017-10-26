3af7dda0501c2ea86096a58a2c61848daad078c1
Phasstw <phasstw@gmail.com>
2017-10-26 10:43:06 -0400

    more work on apply skeleton

--- page-apply.php
+++ page-apply.php
@@ -1,48 +1,61 @@
 <?php get_header(); ?>
 <section class="apply">
   <div class="application-step">
-    <div class="number offset">1</div>
+    <div class="number offset num-1">1</div>
     <div class="info">
-      <h2>Step One</h2>
+      <h2><span>S</span>tep <span>O</span>ne</h2>
       <p>
         <span class="italic">The application is where you begin.</span> Download the application below, and fill out all of the necessary fields. Make sure to enter your personal details, and where necessary do lorem ipsum. Vestibulum rutrum quam vitae fringilla tincidunt. Ut laoreet sodales nisi, quis iaculis nulla iaculis vitae. 
       </p>
-      <a class="application-dl">Download Application</a>
+      <a href="#" class="application-dl">Download Application</a>
     </div>
   </div>
 
   <div class="application-step offset">
-    <div class="number">2</div>
+    <div class="number num-2">2</div>
     <div class="info">
-      <h2>Step Two</h2>
+      <h2><span>S</span>tep <span>T</span>wo</h2>
       <p>
         <span class="italic">Next you need two references.</span> Download both reference forms below. One will be your personal reference letter, and the other a reference letter from your pastor. Lorem ipsum dolor sit amet. Arma virumque cano. Duis tincidunt urna non pretium porta. Nam condimentum vitae ligula vel ornare. 
       </p>
-      <a class="application-dl">Personal Reference Letter</a>
-      <a class="application-dl">Personal Reference Letter</a>
+      <a href="#" class="application-dl">Personal Reference Letter</a>
+      <a href="#" class="application-dl">Personal Reference Letter</a>
     </div>
   </div>
 
   <div class="application-step">
-    <div class="number offset">3</div>
+    <div class="number offset num-3">3</div>
     <div class="info">
-      <h2>Step Three</h2>
+      <h2><span>S</span>tep <span>T</span>hree</h2>
       <p>
         <span class="italic">Then write your essays.</span> Here is where you get a chance to express what motivates you to apply, your values and life goals, as well as the academic interests you would like to pursue. Vestibulum rutrum quam vitae fringilla tincidunt. Ut laoreet sodales nisi, quis iaculis nulla iaculis vitae.  
       </p>
-      <a class="application-dl">Essay Requirements</a>
+      <a href="#" class="application-dl">Essay Requirements</a>
     </div>
   </div>
 
   <div class="application-step offset">
-    <div class="number">4</div>
+    <div class="number num-4">4</div>
     <div class="info">
-      <h2>Step Four</h2>
+      <h2><span>S</span>tep <span>F</span>our</h2>
       <p>
         <span class="italic">Finally send us your transcripts.</span> Use a form such as the one below to request transcripts from the academic institutions you’ve previously studied at. Lorem ipsum dolor sit amet. Arma virumque cano. Duis tincidunt urna non pretium porta. Nam condimentum vitae ligula vel ornare.
       </p>
-      <a class="application-dl">Transcript Request Form</a>
+      <a href="#" class="application-dl">Transcript Request Form</a>
     </div>
   </div>
+  <div class="banner-connect-large">
+    <h3>Connect with us</h3>
+    <div id="frame">
+      <p>Discuss degree options, explore financial aid, and start the conversation:</p>
+      <a class="button" href="/contact">Contact</a>
+    </div>
+  </div>
+  <h2 id="residency">Residency & Time Requirements</h2>
+  <ul>
+    <li>The last 45 hours of credit toward the Diploma or Master of Divinity must be earned at The Reformed Episcopal Seminary.</li>
+    <li>Any degree from RES must be completed within seven years of original matriculation. Upon appeal to the Academic Affairs Committee and under unusual circumstances, one extension period of three years may be given.</li>
+  </ul>
+  <div class="bottom-photo"></div>
 </section>
 <?php get_footer(); ?>
